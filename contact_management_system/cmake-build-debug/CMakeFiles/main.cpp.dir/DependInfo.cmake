d4445aea59d8df9c35f6d25d5b2314c8b4d1f712
Giovani Casia <giovanicasia@gmail.com>
2024-04-06 17:09:59 -0600

    hash

---
+++ contact_management_system/cmake-build-debug/CMakeFiles/main.cpp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/giovanic/Documentos/Tareas/1S 2024/EDD/practica2EDD/contact_management_system/grupo/Contacto.cpp" "CMakeFiles/main.cpp.dir/grupo/Contacto.cpp.o" "gcc" "CMakeFiles/main.cpp.dir/grupo/Contacto.cpp.o.d"
+  "/home/giovanic/Documentos/Tareas/1S 2024/EDD/practica2EDD/contact_management_system/main.cpp" "CMakeFiles/main.cpp.dir/main.cpp.o" "gcc" "CMakeFiles/main.cpp.dir/main.cpp.o.d"
+  "/home/giovanic/Documentos/Tareas/1S 2024/EDD/practica2EDD/contact_management_system/util/Scan.cpp" "CMakeFiles/main.cpp.dir/util/Scan.cpp.o" "gcc" "CMakeFiles/main.cpp.dir/util/Scan.cpp.o.d"
+  "/home/giovanic/Documentos/Tareas/1S 2024/EDD/practica2EDD/contact_management_system/util/Token.cpp" "CMakeFiles/main.cpp.dir/util/Token.cpp.o" "gcc" "CMakeFiles/main.cpp.dir/util/Token.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ contact_management_system/cmake-build-debug/CMakeFiles/main.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.cpp.dir/grupo/Contacto.cpp.o"
+  "CMakeFiles/main.cpp.dir/grupo/Contacto.cpp.o.d"
+  "CMakeFiles/main.cpp.dir/main.cpp.o"
+  "CMakeFiles/main.cpp.dir/main.cpp.o.d"
+  "CMakeFiles/main.cpp.dir/util/Scan.cpp.o"
+  "CMakeFiles/main.cpp.dir/util/Scan.cpp.o.d"
+  "CMakeFiles/main.cpp.dir/util/Token.cpp.o"
+  "CMakeFiles/main.cpp.dir/util/Token.cpp.o.d"
+  "main.cpp"
+  "main.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
